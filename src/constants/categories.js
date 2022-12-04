f0e98ab3d3cbe9324f33edf9ff6da35ac5c8effc
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-04 19:31:04 +0200

    Add the mock data

---
+++ src/constants/categories.js
@@ -0,0 +1,11 @@
+export const CATEGORIES = Object.freeze({
+  abstract: 'Abstract',
+  food: 'Food',
+  flowers: 'Flowers',
+  games: 'Games',
+  space: 'Space',
+  entertainment: 'Entertainment',
+  cars: 'Cars',
+  animals: 'Animals',
+  nature: 'Nature',
+});
---
+++ src/constants/images.js
@@ -0,0 +1,85 @@
+import { v4 as uuidv4 } from 'uuid';
+import { CATEGORIES } from './categories';
+
+export const IMAGES = Object.freeze([
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/47547/squirrel-animal-cute-rodents-47547.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/34231/antler-antler-carrier-fallow-deer-hirsch.jpg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/45911/peacock-plumage-bird-peafowl-45911.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/17811/pexels-photo.jpg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/1108099/pexels-photo-1108099.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/33787/chimpanzee-sitting-sad-mammal.jpg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/326012/pexels-photo-326012.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/847393/pexels-photo-847393.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/567540/pexels-photo-567540.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/572861/pexels-photo-572861.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/320014/pexels-photo-320014.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/1499596/pexels-photo-1499596.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/247399/pexels-photo-247399.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/34770/haan-crows-mohawk.jpg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/598966/pexels-photo-598966.jpeg',
+    categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg',
+    categories: [CATEGORIES.animals],
+  },
+]);
