7d77c9e0d36f900f6160f037710eb06ea2cf7bcc
Yevhen Bodnia <bodnya29179@gmail.com>
2022-12-09 01:51:14 +0200

    Add new images

--- src/constants/images.js
+++ src/constants/images.js
@@ -9,8 +9,23 @@ export const IMAGES = Object.freeze([
   },
   {
     id: uuidv4(),
+    url: 'https://images.pexels.com/photos/541484/sun-flower-blossom-bloom-pollen-541484.jpeg',
+    categories: [CATEGORIES.flowers],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/1493226/pexels-photo-1493226.jpeg',
+    categories: [CATEGORIES.abstract],
+  },
+  {
+    id: uuidv4(),
     url: 'https://images.pexels.com/photos/34231/antler-antler-carrier-fallow-deer-hirsch.jpg',
     categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/534285/pexels-photo-534285.jpeg?auto=compress',
+    categories: [CATEGORIES.food],
   },
   {
     id: uuidv4(),
@@ -44,8 +59,18 @@ export const IMAGES = Object.freeze([
   },
   {
     id: uuidv4(),
+    url: 'https://images.pexels.com/photos/533685/pexels-photo-533685.jpeg',
+    categories: [CATEGORIES.cars],
+  },
+  {
+    id: uuidv4(),
     url: 'https://images.pexels.com/photos/567540/pexels-photo-567540.jpeg',
     categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/2156883/pexels-photo-2156883.jpeg',
+    categories: [CATEGORIES.abstract],
   },
   {
     id: uuidv4(),
@@ -64,6 +89,11 @@ export const IMAGES = Object.freeze([
   },
   {
     id: uuidv4(),
+    url: 'https://images.pexels.com/photos/533978/pexels-photo-533978.jpeg',
+    categories: [CATEGORIES.nature],
+  },
+  {
+    id: uuidv4(),
     url: 'https://images.pexels.com/photos/247399/pexels-photo-247399.jpeg',
     categories: [CATEGORIES.animals],
   },
@@ -79,7 +109,37 @@ export const IMAGES = Object.freeze([
   },
   {
     id: uuidv4(),
+    url: 'https://images.pexels.com/photos/533562/pexels-photo-533562.jpeg',
+    categories: [CATEGORIES.cars],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/533931/pexels-photo-533931.jpeg',
+    categories: [CATEGORIES.nature],
+  },
+  {
+    id: uuidv4(),
     url: 'https://images.pexels.com/photos/36347/cow-pasture-animal-almabtrieb.jpg',
     categories: [CATEGORIES.animals],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/2471234/pexels-photo-2471234.jpeg',
+    categories: [CATEGORIES.abstract],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/533186/pexels-photo-533186.jpeg',
+    categories: [CATEGORIES.food],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/532891/pexels-photo-532891.jpeg',
+    categories: [CATEGORIES.entertainment],
+  },
+  {
+    id: uuidv4(),
+    url: 'https://images.pexels.com/photos/1029611/pexels-photo-1029611.jpeg',
+    categories: [CATEGORIES.abstract],
   },
 ]);
